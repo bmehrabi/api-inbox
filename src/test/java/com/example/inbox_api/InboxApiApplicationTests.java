0efd9c95732efb4a1104da574e1d1c719b6466b9
Babak Mehrabi <b.mehrabi.91@gmail.com>
2025-12-18 12:20:51 +0100

    project setup

---
+++ src/test/java/com/example/inbox_api/InboxApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.inbox_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InboxApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
