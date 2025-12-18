86ec0228d23b30133b5139c278546e0c28199b73
Babak Mehrabi <b.mehrabi.91@gmail.com>
2025-12-18 20:37:30 +0100

    Adding CORS support to MessageController

--- src/main/java/com/example/inbox_api/controller/MessageController.java
+++ src/main/java/com/example/inbox_api/controller/MessageController.java
@@ -9,6 +9,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api/messages")
 @RequiredArgsConstructor
